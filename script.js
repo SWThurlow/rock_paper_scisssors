8581fdba7c9c30023d0fc244f127f49d803c8560
Samuel Thurlow <thurlow.webstuff@gmail.com>
2021-05-06 14:56:57 +0100

    ADD CONTROLS IN WINDOW NOT THE CONSOLE
    Added html elements to allow the game to be played outside of the console.
    
    Changed the structure of the javascript to allow for html controls to be used in a manner that should make
    sense to the player.

--- script.js
+++ script.js
@@ -1,51 +1,58 @@
-//Variables for counting the score.
+//Variables for counting the score, and keeping tack of player choice.
 let playerWins = 0;
 let computerWins = 0;
 let draws =  0;
+let playerChoice = 0;
+let totalRounds = 0;
+let lastChoice;
+
+//Retrieving the necessary elements.
+const gameStart = document.querySelector('.start');
+const rock = document.querySelector('.rock');
+const paper  = document.querySelector('.paper');
+const scissors =  document.querySelector('.scissors');
+const comentary =  document.querySelector('.runningComentary');
+const playLog = document.querySelector('.playLog')
+
+//Event Listeners for game start.
+gameStart.addEventListener('click', game);
+
 
 //Computer's choice.
 function computerSelection(){
     const options = ["rock", "paper", "scissors"];
     //Random number from 0 - 2 to pick from array.
     let choice = Math.floor(Math.random() * 3);
-    console.log(`Computer: ${options[choice]}`)
+    if(lastChoice === options[choice]){
+        computerSelection();
+    }
+    lastChoice = options[choice];
     return options[choice]
-}
-
-//Players choice.
-function playerSelection(){
-    //Getting the players choice and sorting out and case sensitivity issues.
-    let choice = prompt("Rock, Paper, Scissors?");
-    choice.toLowerCase();
-    //Checking the player has picked a valid game option.
-    if(choice !== "rock" && choice !== "paper" && choice !== "scissors"){
-        console.log(`${choice} is not a valid move.`);
-        choice = prompt("Rock, Paper, Scissors?");
-        choice.toLowerCase();
-        }
-    console.log(`Player: ${choice}`)
-    return choice;
 }
 
 //Game logic, comparing the return values from playerSelection and computerSelection.
 function playRound(){
     //Had to put playerChoice first to remove player being able to see the computerSelection output.
-    let playerChoice = playerSelection();
     let compChoice = computerSelection();
+
+    //Tally of round played increase.
+    totalRounds++
+
+    //Figuring out who won this round.
     if(compChoice === playerChoice){
-        console.log("Draw");
+        comentary.innerHTML = `Draw!`;
         draws++;
         return 
     } else if((compChoice === "rock" && playerChoice === "scissors") || 
                 (compChoice === "scissors" && playerChoice === "paper") || 
                 (compChoice === "paper" && playerChoice === "rock")){
-                    console.log("Computer Wins!");
+                    comentary.innerHTML = `The computer wins this round.`;
                     computerWins++;
                     return
     }else if((playerChoice === "rock" && compChoice === "scissors") || 
                 (playerChoice === "scissors" && compChoice === "paper") || 
                 (playerChoice === "paper" && compChoice === "rock")){
-                    console.log("Player Wins!");
+                    comentary.innerHTML = `You won this one!`;
                     playerWins++;
                     return
     }
@@ -54,23 +61,85 @@ function playRound(){
 //Game to decide the best of five.
 function game(){
     //Reseting if multiple games are played.
-    computerWins = 0;
     playerWins = 0;
-    draws = 0;
-    //Looping over five times.
-    for(let i = 0; i <= 4; i++){
+    computerWins = 0;
+    draws =  0;
+    totalRounds = 0;
+
+
+    //Prompting the player.
+    comentary.innerHTML = `You move`;
+
+    //Event listeners for player options.
+    rock.addEventListener('click', () => {
+        playerChoice = "rock";
         playRound();
-        console.log(`Draws: ${draws}, Computer Wins: ${computerWins}, Player Wins: ${playerWins}`);
-    }
-    //Deciding the overall winner.
+        if(totalRounds === 5){
+            endGame();
+            return;
+        }
+    });
+    paper.addEventListener('click', () => {
+        playerChoice = "paper";
+        playRound();
+        if(totalRounds === 5){
+            endGame();
+            return;
+        }
+    });
+    scissors.addEventListener('click', () =>{
+        playerChoice = "scissors";
+        playRound();
+        if(totalRounds === 5){
+            endGame();
+            return;
+        }
+    });
+}
+
+//Ending the game and finding out the winner.
+function endGame(){
+    //Finding the winner.
     if(playerWins === computerWins){
-        console.log("It's a draw!");
-        return `Draws: ${draws}, Computer Wins: ${computerWins}, Player Wins: ${playerWins}`;
+        comentary.innerHTML = `The game's a draw!`;
+        let endEntry = document.createElement('p');
+        endEntry.innerHTML = `Draws: ${draws}, Computer Wins: ${computerWins}, Player Wins: ${playerWins}`;
+        playLog.appendChild(endEntry);
     } else if(playerWins > computerWins){
-        console.log("Player wins!");
-        return `Player Wins: ${playerWins}, Computer Wins: ${computerWins}, Draws: ${draws}`;
+        comentary.innerHTML = `You won the game!`;
+        let endEntry = document.createElement('p');
+        endEntry.innerHTML = `Player Wins: ${playerWins}, Computer Wins: ${computerWins}, Draws: ${draws}`;
+        playLog.appendChild(endEntry);
     } else {
-        console.log("Computer Wins!");
-        return `Computer Wins: ${computerWins}, Player Wins: ${playerWins}, Draws: ${draws}`;
+        comentary.innerHTML = `And the computer wins the game!`;
+        let endEntry = document.createElement('p');
+        endEntry.innerHTML = `Computer Wins: ${computerWins}, Player Wins: ${playerWins}, Draws: ${draws}`;
+        playLog.appendChild(endEntry);
     }
+
+    //Removing event listeners so that the game doesnt keep going.
+    rock.removeEventListener('click', () => {
+        playerChoice = "rock";
+        playRound();
+        if(totalRounds === 5){
+            endGame();
+            return;
+        }
+    });
+    paper.removeEventListener('click', () => {
+        playerChoice = "paper";
+        playRound();
+        if(totalRounds === 5){
+            endGame();
+            return;
+        }
+    });
+    scissors.removeEventListener('click', () =>{
+        playerChoice = "scissors";
+        playRound();
+        if(totalRounds === 5){
+            endGame();
+            return;
+        }
+    });
 }
