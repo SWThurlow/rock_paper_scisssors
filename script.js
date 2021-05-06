037e39d1ec0a2ed79306b2e30cf183c8694bc463
Samuel Thurlow <thurlow.webstuff@gmail.com>
2021-05-06 08:37:38 +0100

    ADD COMPUTER AND PLAYER CHOICES
    Computer selection added with use of a random number to choose from an array.
    
    Player promted and the choice checked to see if its a valid gane option.

--- script.js
+++ script.js
@@ -2,3 +2,27 @@
 let playerWins = 0;
 let computerWins = 0;
 let draws =  0;
+
+//Computer's choice.
+function computerSelection(){
+    const options = ["rock", "paper", "scissors"];
+    //Random number from 0 - 2 to pick from array.
+    let choice = Math.floor(Math.random() * 3);
+    console.log(`Computer: ${options[choice]}`)
+    return options[choice]
+}
+
+//Players choice.
+function playerSelection(){
+    //Getting the players choice and sorting out and case sensitivity issues.
+    let choice = prompt("Rock, Paper, Scissors?");
+    choice.toLowerCase();
+    //Checking the player has picked a valid game option.
+    if(choice !== "rock" && choice !== "paper" && choice !== "scissors"){
+        console.log(`${choice} is not a valid move.`);
+        playerSelection()
+        return
+        }
+    console.log(choice)
+    return choice;
+}
